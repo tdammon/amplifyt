2d552b34c8be7f574b2777f82de7b135bbf6b5e3
The Goose <trevordammon@gmail.com>
2020-05-03 12:20:28 -0500

    .

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-
+import {API} from 'aws-amplify'
 
 class App extends Component {
+
+  triggerAPI = () => {
+    API.get('apitest', '/apitest', {})
+    .then(response=> {console.log(response)})
+    .catch(error=> {console.log(error.response)})
+  }
 
   render() {
       return(
         <div>
             <p>Hello</p>
-            <button onClick={()=> axios.post('/apitestlambda-amplifyt', {users:5})}>
+            <button onClick={()=> this.triggerAPI()}>
               Hi
             </button>
         </div>
